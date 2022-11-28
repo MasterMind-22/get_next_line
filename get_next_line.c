40bd6d01fdb74c6e5941631ccc121fc55863ab24
Youssef Nadry <115162045+MasterMind-22@users.noreply.github.com>
2022-11-28 18:56:04 +0100

    1

--- get_next_line.c
+++ get_next_line.c
@@ -1,63 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonadry <yonadry@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 18:44:11 by yonadry           #+#    #+#             */
+/*   Updated: 2022/11/28 18:48:01 by yonadry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-
-char *read_fd(int fd)
+char	*read_fd(int fd, char *read_line)
 {
-	int read_int;
-	char *read_line;
-	char *save_line;
+	int		read_int;
+	char	*from_read;
 
 	read_int = 1;
-	save_line = NULL;
-	read_line = malloc(BUFFER_SIZE + 1);
-	if(!read_line)
+	from_read = malloc(BUFFER_SIZE + 1);
+	if (!from_read)
 		return (NULL);
-	while (!newline_found(read_line) && read_int)
+	while (newline_found(read_line) && read_int)
 	{
-		read_int = read(fd, read_line, BUFFER_SIZE);
-		if (read_int == -1)
-		{
-			free (read_line);
-			return (NULL);
-		}
-		read_line[BUFFER_SIZE] = '\0';
-		if (save_line == NULL)
-			save_line = ft_strdup(read_line);
-		else
-			save_line = ft_strjoin(save_line, read_line);
+		read_int = read(fd, from_read, BUFFER_SIZE);
+		if (read_int == -1 || (read_int == 0 && !read_line))
+			return (free(from_read), free(read_line), NULL);
+		from_read[read_int] = '\0';
+		read_line = ft_strjoin(read_line, from_read);
 	}
-	free(read_line);
-	return (save_line);
-}
-char *return_line_f(char *read_line)
-{
-	int i;
-	char *return_line;
-
-	i = 0;
-	return_line = malloc(newline_found(read_line) + 2);
-	while (read_line[i] != '\n')
+	free(from_read);
+	if (read_line[0] == '\0')
 	{
+		free(read_line);
+		return (NULL);
+	}
+	return (read_line);
+}
+
+char	*return_line_f(char *read_line)
+{
+	int		i;
+	char	*return_line;
+
+	i = -1;
+	while (read_line[++i])
+	{
+		if (read_line[i] == '\n')
+		{
+			i++;
+			break ;
+		}
+	}
+	return_line = (char *)malloc((i + 1) * sizeof(char));
+	if (!return_line)
+		return (NULL);
+	i = -1;
+	while (read_line[++i] && read_line[i] != '\n')
 		return_line[i] = read_line[i];
+	if (read_line[i] == '\n')
+	{
+		return_line[i] = '\n';
 		i++;
 	}
-	if (newline_found(read_line))
-		return_line[i] = '\n';
-	return_line[i + 1] = '\0';
-	return (return_line);
-}
-char *get_next_line(int fd)
-{
-	char *return_line;
-	char *read_line;
-	
-	read_line = read_fd(fd);
-	return_line = return_line_f(read_line);
+	return_line[i] = '\0';
 	return (return_line);
 }
 
-int main(void)
+char	*save_line_f(char *read_line)
 {
-	int fd = open("file", O_RDONLY);
-	printf("|%s|", get_next_line(fd));
+	int		i;
+	char	*save_line;
+	int		j;
+
+	j = 0;
+	i = -1;
+	while (read_line[++i])
+	{
+		if (read_line[i] == '\n')
+		{
+			i++;
+			break ;
+		}
+	}
+	save_line = malloc(ft_strlen(&read_line[i]) + 1);
+	if (!save_line)
+		return (free(read_line), NULL);
+	while (read_line[i])
+	{
+		save_line[j] = read_line[i];
+		i++;
+		j++;
+	}
+	save_line[j] = '\0';
+	return (free(read_line), save_line);
 }
+
+char	*get_next_line(int fd)
+{
+	char		*return_line;
+	static char	*read_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	read_line = read_fd(fd, read_line);
+	if (!read_line)
+		return (NULL);
+	return_line = return_line_f(read_line);
+	read_line = save_line_f(read_line);
+	return (return_line);
+}
+
+// int main(void)
+// {
+// 	int fd = open("file", O_RDONLY);
+// 	char *a= get_next_line(fd);
+// 	printf("%s", a);
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+
+// 	system("leaks a.out");
+
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonadry <yonadry@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/28 18:50:22 by yonadry           #+#    #+#             */
+/*   Updated: 2022/11/28 18:51:00 by yonadry          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-size_t ft_strlen(char *s)
+size_t	ft_strlen(char *s)
 {
 	size_t	i;
 
@@ -12,29 +24,34 @@ size_t ft_strlen(char *s)
 	}
 	return (i);
 }
-int newline_found(char *read_line)
+
+int	newline_found(char *read_line)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (read_line[i])
+	if (!read_line)
+		return (1);
+	while (read_line[i] != '\0')
 	{
 		if (read_line[i] == '\n')
-			return (i);
+			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
 
-char *ft_strjoin(char *s1, char *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
-	int i;
-	int j;
-	char *sum;
+	int		i;
+	int		j;
+	char	*sum;
 
 	i = -1;
 	j = 0;
-	if (!s1 || !s2)
+	if (!s1)
+		s1 = ft_strdup("");
+	if (!s2)
 		return (NULL);
 	sum = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!sum)
@@ -44,6 +61,7 @@ char *ft_strjoin(char *s1, char *s2)
 	while (s2[j])
 		sum[i++] = s2[j++];
 	sum[i] = '\0';
+	free (s1);
 	return (sum);
 }
 
@@ -55,11 +73,14 @@ char	*ft_strdup(char *s1)
 
 	i = 0;
 	if (!s1)
-		return(NULL);
+		return (NULL);
 	len = ft_strlen(s1);
 	copy = malloc(len + 1);
 	if (!copy)
+	{
+		free(s1);
 		return (NULL);
+	}
 	while (s1[i] != '\0')
 	{
 		copy[i] = s1[i];
